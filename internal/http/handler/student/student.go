1ceaada505fbb870d82b505453ab9c0c6c7ef0a1
ageniuscoder <23bcs059@nith.ac.in>
2025-08-20 20:18:06 +0530

    add student list

--- internal/http/handler/student/student.go
+++ internal/http/handler/student/student.go
@@ -71,3 +71,17 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 		response.WriteJson(w, http.StatusOK, student)
 	}
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("getting all student Info")
+
+		students, err := storage.GetStudents()
+
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+		}
+
+		response.WriteJson(w, http.StatusOK, students)
+	}
+}
